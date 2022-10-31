9b2ce1c1f0c96dc89d0c66622caed4b8cf3ad0ae
lediraison-a <alban.lediraison@gmail.com>
2022-10-31 09:18:25 +0100

    init api project

---
+++ projetisenapi/src/main/java/com/isen/projetisenapi/ProjetisenapiApplication.java
@@ -0,0 +1,13 @@
+package com.isen.projetisenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetisenapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetisenapiApplication.class, args);
+	}
+
+}
