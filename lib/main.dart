bb31cad31f86ac8e9ef51384ef322ce45df37199
faniryxx <faniry597@gmail.com>
2022-11-02 15:27:40 +0100

    Cleaned unnecessary comments & code

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'validator.dart'; //email & password validator
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,15 +23,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'MyHomePage',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.green,
       ),
       home: const MyHomePage(title: 'gl_flutter demo'),
@@ -42,16 +32,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -93,7 +73,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   final usernameTextController = TextEditingController();
   final passwordTextController = TextEditingController();
-  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -164,6 +143,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
                     if (firebaseUser == null) {
                       print("firebaseUser null");
+                      // ignore: use_build_context_synchronously
                       Navigator.push(
                           context,
                           MaterialPageRoute(
@@ -173,6 +153,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       print(
                           "firebaseUser ${usernameTextController.text} connected");
                       MyApp.firebaseUser = firebaseUser;
+                      // ignore: use_build_context_synchronously
                       Navigator.push(
                           context,
                           MaterialPageRoute(
@@ -247,9 +228,6 @@ class _ScanScreenState extends State<ScanScreen> {
 
   // Continuous scan
   Future<void> startBarcodeScanStream() async {
-    /*FlutterBarcodeScanner.getBarcodeStreamReceiver(
-        '#ff6666', 'Cancel', true, ScanMode.BARCODE)!
-        .listen((barcode) => print(barcode));*/
     FlutterBarcodeScanner.getBarcodeStreamReceiver(
             '#ff6666', 'Annuler', true, ScanMode.BARCODE)!
         .listen((barcode) => Fluttertoast.showToast(
@@ -269,7 +247,7 @@ class _ScanScreenState extends State<ScanScreen> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-              Text("Bonjour ${MyApp.firebaseUser?.email}"),
+              Text("Bonjour ${MyApp.firebaseUser?.uid}"),
               Text("Contenu du code-barres: $_scanBarcode"),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
