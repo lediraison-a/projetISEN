0fadec7265cc2363f2c5f7ead6403fa0fb6e98a6
lediraison-a <alban.lediraison@gmail.com>
2022-11-02 11:34:28 +0100

    openfood dao get product

--- projetisenapi/src/main/java/com/isen/projetisenapi/dao/openfood/OpenFoodDao.java
+++ projetisenapi/src/main/java/com/isen/projetisenapi/dao/openfood/OpenFoodDao.java
@@ -1,7 +1,6 @@
 package com.isen.projetisenapi.dao.openfood;
 
-import org.springframework.stereotype.Repository;
-
-@Repository
 public interface OpenFoodDao {
+
+    String getProduct(String barcode);
 }
---
+++ projetisenapi/src/main/java/com/isen/projetisenapi/utils/mappers/Product.java
@@ -0,0 +1,34 @@
+package com.isen.projetisenapi.utils.mappers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+public class Product {
+
+    private final JsonNode rootNode;
+
+    public Product(String productJson) throws JsonProcessingException {
+        rootNode = new ObjectMapper().readTree(productJson);
+    }
+
+    public String getProductName() {
+        return rootNode.get("product").get("product_name").asText();
+    }
+
+    public String getBarcode() {
+        return rootNode.get("code").asText();
+    }
+
+    public List<String> getAllergens() {
+        return StreamSupport
+                .stream(rootNode.get("product").get("allergens_tags").spliterator(), false)
+                .map(JsonNode::asText)
+                .collect(Collectors.toList());
+    }
+
+}
