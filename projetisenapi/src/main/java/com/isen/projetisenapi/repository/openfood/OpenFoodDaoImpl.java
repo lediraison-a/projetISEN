458e146f3194124f0d69c2c415017fb052e83952
lediraison-a <alban.lediraison@gmail.com>
2022-11-25 14:08:52 +0100

    app loggs

--- projetisenapi/src/main/java/com/isen/projetisenapi/repository/openfood/OpenFoodDaoImpl.java
+++ projetisenapi/src/main/java/com/isen/projetisenapi/repository/openfood/OpenFoodDaoImpl.java
@@ -1,5 +1,8 @@
 package com.isen.projetisenapi.repository.openfood;
 
+import com.isen.projetisenapi.repository.firestore.FirestoreDaoImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,6 +10,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Repository
 public class OpenFoodDaoImpl implements OpenFoodDao {
 
+    private static final Logger LOG = LoggerFactory.getLogger(FirestoreDaoImpl.class);
     private final WebClient webClient;
 
     public OpenFoodDaoImpl(@Value("${dao.openfood.url}") String apiBaseUri) {
@@ -15,6 +19,7 @@ public class OpenFoodDaoImpl implements OpenFoodDao {
 
     @Override
     public String getProduct(String barcode) {
+        LOG.info("getProduct {}", barcode);
         String GET_PRODUCT_URI = "/api/v2/product/{barcode}";
         return webClient.get()
                 .uri(GET_PRODUCT_URI, barcode)
--- projetisenapi/src/main/java/com/isen/projetisenapi/service/ProductService.java
+++ projetisenapi/src/main/java/com/isen/projetisenapi/service/ProductService.java
@@ -3,11 +3,14 @@ package com.isen.projetisenapi.service;
 import com.isen.projetisenapi.models.ProductInfo;
 import com.isen.projetisenapi.repository.openfood.OpenFoodDao;
 import com.isen.projetisenapi.utils.mapper.ProductMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ProductService.class);
     private final OpenFoodDao openFoodDao;
 
     public ProductService(OpenFoodDao openFoodDao) {
@@ -15,11 +18,12 @@ public class ProductService {
     }
 
     public ProductInfo getProductInfo(String barcode) {
+        LOG.info("getProductInfo {}", barcode);
         ProductInfo productInfo = null;
         try {
             productInfo = ProductMapper.getProductInfo(openFoodDao.getProduct(barcode));
         } catch (Exception e) {
-
+            LOG.error(e.getMessage());
         }
         return productInfo;
     }
