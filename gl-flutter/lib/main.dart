c25b1ab3bb304f857fa77eaf56869c9b20a267af
faniryxx <faniry597@gmail.com>
2023-03-03 11:30:44 +0100

    Changed from IP to hostname when opening front

--- gl-flutter/lib/main.dart
+++ gl-flutter/lib/main.dart
@@ -772,8 +772,8 @@ class AllergenDetailsScreen extends StatelessWidget {
                                           BorderRadius.circular(15)))),
                       onPressed: () async {
                         // For some reason, access with hostname is random
-                        //final Uri _url = Uri.parse('http://vps-6d160f31.vps.ovh.net/');
-                        final Uri _url = Uri.parse('http://54.36.181.29/');
+                        final Uri _url = Uri.parse('http://vps-6d160f31.vps.ovh.net/');
+                        //final Uri _url = Uri.parse('http://54.36.181.29/');
                         if (!await launchUrl(_url, mode: LaunchMode.inAppWebView)) {
                           throw Exception('Could not launch $_url');
                         }
